a8d75aedec51aebb69b5941af299d0ba33aa16ce
Crowdbotics <team@crowdbotics.com>
2023-04-06 01:29:28 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wandering-waterfall-39739",
+  url: "https://wandering-waterfall-39739.botics.co",
+  api: "https://wandering-waterfall-39739.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
